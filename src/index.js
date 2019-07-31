d1a4274d55f7f4bb6d60944e3e1ceb4d7f1b40ec
Sergey Zhigunov <i.sergey.zhigunov@gmail.com>
2019-07-31 16:41:44 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Shepherd from 'shepherd.js'
 
-import 'shepherd.js/dist/css/shepherd-theme-default.css'
-
 const internalMethods = [
   'back',
   'cancel',
